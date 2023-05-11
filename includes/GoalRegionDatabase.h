91102701da69bc4d070c166dce78bfc2a3578590
Justin Kottinger <justin.kottinger@colorado.edu>
2023-05-11 11:11:17 -0600

    allow for floating point goals

--- includes/GoalRegionDatabase.h
+++ includes/GoalRegionDatabase.h
@@ -36,14 +36,15 @@
 
 #include <ompl/base/goals/GoalRegion.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
+#include <utility>
 
 namespace ob = ompl::base;
 
 class GoalRegion2ndOrderCar: public ob::GoalRegion
 {
 public:
-    GoalRegion2ndOrderCar(const ob::SpaceInformationPtr &si, std::pair<int, int> goal): 
-        ob::GoalRegion(si), gx_(goal.first), gy_(goal.second)
+    GoalRegion2ndOrderCar(const ob::SpaceInformationPtr &si, double gx, double gy): 
+        ob::GoalRegion(si), gx_(gx), gy_(gy)
     {
         threshold_ = 0.25;
     }
@@ -54,6 +55,6 @@ public:
         return sqrt(pow(robot_pos[0] - gx_, 2) + pow(robot_pos[1] - gy_, 2));
     }
 private:
-    int gx_;
-    int gy_;
+    const double gx_;
+    const double gy_;
 };
